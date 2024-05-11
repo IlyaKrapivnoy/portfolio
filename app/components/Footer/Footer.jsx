f2a66f1feffa2a076734fe1bbb692b131742aa21
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2024-05-11 15:40:25 +0200

    add footer links

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -1,22 +1,53 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import { currentYear } from "@/app/helpers/functions";
+import Email from "../../../public/Email.svg";
+import GitHub from "../../../public/GitHub.svg";
+import LinkedIn from "../../../public/LinkedIn.svg";
 
 const Footer = () => {
-  const currentYear = new Date().getFullYear();
+  const socialLinks = [
+    {
+      svg: LinkedIn,
+      href: "https://linkedin.com/in/ilya-krapivnoy-ba53162aa",
+      text: "LinkedIn",
+    },
+    {
+      svg: GitHub,
+      href: "https://github.com/IlyaKrapivnoy",
+      text: "GitHub",
+    },
+    {
+      svg: Email,
+      href: "mailto:ilya.krapivnoy@gmail.com",
+      text: "ilya.krapivnoy@gmail.com",
+    },
+  ];
 
   return (
-    <footer
-      className={"flex flex-col text-center border-t-[1px] border-white py-16"}
-    >
-      <h2 className={"uppercase text-6xl font-black mb-10"}>Contact</h2>
-      <Link
-        className={"font-extralight mb-1"}
-        href="mailto:ilya.krapivnoy@gmail.com"
-      >
-        ilya.krapivnoy@gmail.com
-      </Link>
-      <p className={"font-extralight opacity-60"}>
-        &copy; {currentYear} All rights reserved
+    <footer className="flex flex-col text-center border-t-[1px] border-white py-16">
+      <h2 className="uppercase text-6xl font-black mb-10">Contact</h2>
+      <div className="mb-6">
+        {socialLinks.map((item) => (
+          <div
+            className="flex items-center justify-center gap-2 mb-2 hover:animate-pulse"
+            key={item.text}
+          >
+            <Image
+              src={item.svg}
+              alt="Send me email icon"
+              width={20}
+              height={20}
+            />
+            <Link className="font-bold" href={item.href}>
+              {item.text}
+            </Link>
+          </div>
+        ))}
+      </div>
+      <p className="font-extralight opacity-60">
+        {currentYear} All rights reserved
       </p>
     </footer>
   );
