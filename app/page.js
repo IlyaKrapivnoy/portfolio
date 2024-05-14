00711fcddfb36fe2a3a6f665f87413ab93ee8eea
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2024-05-14 14:47:28 +0200

    add scroll to top btn

--- app/page.js
+++ app/page.js
@@ -1,7 +1,22 @@
+"use client";
+
+import ScrollToTop from "react-scroll-to-top";
+import Image from "next/image";
+import { Up } from "@/public/svg";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       content
+      <ScrollToTop
+        style={{
+          background: "none",
+          boxShadow: "none",
+        }}
+        className="text-[var(--black)] font-bold sm:mb-20 -mb-6"
+        smooth
+        component={<Image src={Up} alt="arrow" width={40} height={40} />}
+      />
     </main>
   );
 }
--- public/svg/index.js
+++ public/svg/index.js
@@ -1,2 +1,3 @@
 export { default as LinkedIn } from "./LinkedIn.svg";
 export { default as GitHub } from "./GitHub.svg";
+export { default as Up } from "./Up.svg";
